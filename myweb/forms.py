ced4f5a74a228ed4eecb748b018c5e153985b534
jaopang <thanasorn.kr.62@ubu.ac.th>
2020-10-25 18:04:25 +0700

    987

--- myweb/forms.py
+++ myweb/forms.py
@@ -2,9 +2,16 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Destination
+
 
 
 class CreateUserForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
+
+class Destinationform(ModelForm):
+    class Meta:
+        model = Destination
+        fields = ['name','img','price','desc']
--- myweb/views.py
+++ myweb/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from .models import Destination
+from .models import *
+from .forms import Destinationform
 
 
-def index(req):
-	return render(req, 'myweb/index.html' )
 
 def signup(request):
     if request.method == 'POST':
@@ -21,9 +22,23 @@ def signup(request):
         form = UserCreationForm()
     return render(request, 'myweb/register.html', {'form': form})
 
-def post(req):
-    return render(req, 'myweb/post.html')
 
 def aboutcow(req):
     return render(req, 'myweb/aboutcow.html')
-    
+
+def index(req):
+    Destinations = Destination.objects.all()
+    return render(req, 'myweb/index.html', {'Destinations': Destinations})
+
+def post(request):
+    if request.method == 'POST':
+        form = Destinationform(request.POST)
+
+        if form.is_valid():
+            a = form.save()
+            a.save()
+            return redirect("/index")
+    else:
+        form = Destinationform()
+        context = {'form': form}
+        return render(request, 'myweb/post.html', context)
